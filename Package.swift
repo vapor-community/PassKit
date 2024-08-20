7688b592f43a4c18066a8bee9b0cdf0a14420c40
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2024-08-20 11:43:56 +0200

    Bump dependencies

--- Package.swift
+++ Package.swift
@@ -11,10 +11,10 @@ let package = Package(
         .library(name: "Orders", targets: ["Orders"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.102.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.102.1"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.11.0"),
         .package(url: "https://github.com/vapor/apns.git", from: "4.1.0"),
-        .package(url: "https://github.com/vapor-community/Zip.git", from: "2.1.3"),
+        .package(url: "https://github.com/vapor-community/Zip.git", from: "2.2.0"),
         .package(url: "https://github.com/apple/swift-certificates.git", from: "1.5.0"),
         // used in tests
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.7.4"),
